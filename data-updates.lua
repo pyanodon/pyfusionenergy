279d470cc55947a542747724dd2aa4b64a0c6378
pyanodon <smylodon@gmail.com>
2017-08-04 23:34:48 -0300

    First base
    ore added

---
+++ data-updates.lua
@@ -0,0 +1,14 @@
+--update recipes for creosote
+
+--update data for other mods.
+
+--update recipes to use bobmods
+if bobmods and bobmods.lib then
+    require("prototypes.updates.bob-updates")
+    require("prototypes.updates.bob-additions")
+end
+
+--angel-updates
+if angelsmods then
+    require("prototypes.updates.angel-updates")
+end
---
+++ data.lua
@@ -0,0 +1,27 @@
+--Settings and Helpers
+
+require("stdlib.table")
+require("stdlib.string")
+require("stdlib.defines.color")
+require("stdlib.defines.time")
+
+--Groups
+
+--Categories
+
+--Technologies
+
+--Recipes without items
+
+--Items without entities
+require("prototypes.items.items")
+
+--Tiles and Terrain
+
+--Fluids
+
+--Buildings
+
+--ores
+require("prototypes.ore.molybdenum")
+
