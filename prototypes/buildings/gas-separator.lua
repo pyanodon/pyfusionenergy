5d84b16cf2a0dce38601743e135c47fbc9331c72
Nicholas Dunn <nexela@gmail.com>
2017-11-20 23:49:21 -0500

    syntax fixes

--- prototypes/buildings/gas-separator.lua
+++ prototypes/buildings/gas-separator.lua
@@ -109,7 +109,6 @@ local entity1={
             base_level = 1,
             pipe_connections = {{ type="output", position = {-2.0, -4.0} }}
         },
-		
         --North3
         {
             production_type = "output",
@@ -118,7 +117,6 @@ local entity1={
             base_level = 1,
             pipe_connections = {{ type="output", position = {0.0, -4.0} }}
         },
-		
         --South
         {
             production_type = "input",
