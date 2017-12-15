4c7eb043e4864ec48c18c0b205fcac90ca785445
pyanodon <smylodon@gmail.com>
2017-12-15 12:24:59 -0200

    balances

--- prototypes/fluids/heavy-water.lua
+++ prototypes/fluids/heavy-water.lua
@@ -5,7 +5,7 @@ local heavy_water = {
     enabled = "false",
     energy_required = 6,
     ingredients = {
-        {type = "item", name = "sulfur", amount = 10},
+        {type = "item", name = "sulfur", amount = 25},
         {type = "fluid", name = "pressured-water", amount = 1000}
     },
     results = {
