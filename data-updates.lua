f2c4be89a9487c867eb4fc9e010774689f7ea61a
pyanodon <smylodon@gmail.com>
2018-10-30 21:48:29 -0300

    fixes

--- data-updates.lua
+++ data-updates.lua
@@ -1,8 +1,5 @@
 require("stdlib/data/data").create_data_globals()
 
-if mods["boblibrary"] then
-    require("prototypes/updates/bob-updates")
-    require("prototypes/updates/bob-additions")
-end
+
 
 ITEM("production-science-pack", "tool").icon = "__pyfusionenergy__/graphics/icons/production-science-pack.png"
