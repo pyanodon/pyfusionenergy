b87c441f472383ab3ae221305f3847afd52a398c
pyanodon <smylodon@gmail.com>
2018-11-19 12:32:39 -0200

    jig-pipe-fix
    Former-commit-id: 173fb74e5d820288c4381c590fc0222762ce6a93
    Former-commit-id: 5f5bafb19502aba1d10bc15a18182bd6107b2ba4

--- prototypes/buildings/jig.lua
+++ prototypes/buildings/jig.lua
@@ -137,7 +137,7 @@ ENTITY {
     fluid_boxes = {
         {
             production_type = "input",
-            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {1.8, 4.3}, {-1.2, -4.65}, {4.8, -1.7}, {-4.27, 1.3}, pipe_left),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {1.8, 4.3}, {-1.2, -4.65}, {4.8, -1.7}, {-4.27, 1.3}, pipe_right),
             pipe_covers = DATA.Pipes.covers(false, true, false, false),
             base_area = 10,
             base_level = -1,
@@ -147,12 +147,12 @@ ENTITY {
             production_type = "output",
             pipe_covers = DATA.Pipes.covers(false, true, false, false),
             base_level = 1,
-            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {1.8, 4.3}, {-1.2, -4.65}, {4.8, -1.7}, {-4.27, 1.3}, pipe_left),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {1.8, 4.3}, {-1.2, -4.65}, {4.8, -1.7}, {-4.27, 1.3}, pipe_right),
             pipe_connections = {{type = "output", position = {1.5, 4.5}}}
         },
         {
             production_type = "input",
-            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {-1.2, 4.3}, {1.8, -4.65}, {4.8, 1.3}, {-4.27, -1.8}, pipe_right),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {-1.2, 4.3}, {1.8, -4.65}, {4.8, 1.3}, {-4.27, -1.8}, pipe_left),
             pipe_covers = DATA.Pipes.covers(false, true, false, false),
             base_area = 10,
             base_level = -1,
@@ -162,7 +162,7 @@ ENTITY {
             production_type = "output",
             pipe_covers = DATA.Pipes.covers(false, true, false, false),
             base_level = 1,
-            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {-1.2, 4.3}, {1.8, -4.65}, {4.8, 1.3}, {-4.27, -1.8}, pipe_right),
+            pipe_picture = DATA.Pipes.pictures("assembling-machine-2", {-1.2, 4.3}, {1.8, -4.65}, {4.8, 1.3}, {-4.27, -1.8}, pipe_left),
             pipe_connections = {{type = "output", position = {-1.5, 4.5}}}
         },
         off_when_no_fluid_recipe = true
