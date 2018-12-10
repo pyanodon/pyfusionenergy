c63a000deb403be9698e6ed342bb01286f7cf4c7
pyanodon <smylodon@gmail.com>
2018-12-10 20:07:38 -0200

    balance
    Former-commit-id: dce1d0f3862aeda994a6799134e4517909cd932d
    Former-commit-id: 2fd19aad755eb48786e1b3ac8dc29fc9aba0d931

--- prototypes/buildings/heat-exchanger.lua
+++ prototypes/buildings/heat-exchanger.lua
@@ -46,7 +46,7 @@ ENTITY {
     },
     allowed_effects = {"consumption", "speed", "productivity", "pollution"},
     crafting_categories = {"heat-exchanger"},
-    crafting_speed = 1,
+    crafting_speed = 1.5,
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
