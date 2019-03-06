6d59cf83d6cf6cfa81a69024fea044b6a6345982
pyanodon <smylodon@gmail.com>
2019-03-06 13:10:33 -0300

    fixed-techtree
    Former-commit-id: e81c558b81bbfc0587c7a5e1044d02ba53d3e1d8
    Former-commit-id: c9f8bb8667bbdefb760a6fb58e5f8b1d79091fb1

--- data-updates.lua
+++ data-updates.lua
@@ -3,3 +3,15 @@ require("stdlib/data/data").create_data_globals()
 
 
 ITEM("production-science-pack", "tool").icon = "__pyfusionenergy__/graphics/icons/production-science-pack.png"
+
+RECIPE("production-science-pack"):remove_unlock('production-science-pack')
+TECHNOLOGY('production-science-pack'):remove_prereq('advanced-material-processing-2'):remove_prereq('productivity-module'):remove_prereq('railway')
+TECHNOLOGY('automation-3'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('logistics-3'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('coal-liquefaction'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('kovarex-enrichment-process'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('nuclear-fuel-reprocessing'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('speed-module-3'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('productivity-module-3'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('effectivity-module-3'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
+TECHNOLOGY('effect-transmission'):remove_prereq('production-science-pack'):add_prereq('diamond-mining')
