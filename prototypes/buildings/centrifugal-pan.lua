cc4e6ef8cc8816c83a2039e5a1d9ca194c8e51a9
pyanodon <smylodon@gmail.com>
2019-03-18 20:04:13 -0300

    speed balance

--- prototypes/buildings/centrifugal-pan.lua
+++ prototypes/buildings/centrifugal-pan.lua
@@ -100,7 +100,7 @@ ENTITY {
     },
     allowed_effects = {"consumption", "speed", "productivity", "pollution"},
     crafting_categories = {"pan"},
-    crafting_speed = 1,
+    crafting_speed = 1.5,
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
