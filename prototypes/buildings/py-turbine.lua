da92e2b16db82afdf82b0bfe26cd97bd27c980c6
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-03-31 01:51:31 -0400

    update emissions
    updated old emissions to emissions per second per watt before the old way was broken by factorio
    
    
    Former-commit-id: 3498506b42c0d0267b8b86cfdf81ec2d5f1a8f63
    Former-commit-id: fb5dc456a5bf6415d6acec22da17ccc058139cc1

--- prototypes/buildings/py-turbine.lua
+++ prototypes/buildings/py-turbine.lua
@@ -66,7 +66,7 @@ ENTITY {
     energy_source = {
         type = "electric",
         usage_priority = "secondary-output",
-        emissions = 0.001,
+        emissions_per_second_per_watt = 1e-06,
     },
     --scale=1.75,
     horizontal_animation = {
