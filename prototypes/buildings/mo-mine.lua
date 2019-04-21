2c23219d3ae68109577f015393ff07de17689515
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-04-20 22:38:42 -0400

    lowered pullotion back down to a reasonable amount
    Former-commit-id: f1f177cf40e7b401d91ffc1387dae3eeb8db08e4
    Former-commit-id: 93a799a0f6757e3099c284af377b6fd93cd80c71

--- prototypes/buildings/mo-mine.lua
+++ prototypes/buildings/mo-mine.lua
@@ -54,7 +54,7 @@ ENTITY {
       fuel_category = "drill",
       effectivity = 8,
       fuel_inventory_size = 1,
-      emissions_per_minute = 50,
+      emissions_per_minute = 0.06,
       smoke =
       {
         {
--- prototypes/buildings/py-turbine.lua
+++ prototypes/buildings/py-turbine.lua
@@ -66,7 +66,7 @@ ENTITY {
     energy_source = {
         type = "electric",
         usage_priority = "secondary-output",
-        emissions_per_minute = 50,
+        emissions_per_minute = 0.06,
     },
     --scale=1.75,
     horizontal_animation = {
