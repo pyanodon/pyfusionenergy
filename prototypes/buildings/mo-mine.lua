86382662692ff0115ce9a8ac399595079085e9c9
pyanodon <smylodon@gmail.com>
2019-05-10 18:34:05 -0300

    fixed-mo mine
    Former-commit-id: c50a962ef802d1056c5b11cbfbaa03cb52498371
    Former-commit-id: d89051ee4733779dc311f9119a1b9b86d5f7d5af

--- prototypes/buildings/mo-mine.lua
+++ prototypes/buildings/mo-mine.lua
@@ -72,7 +72,7 @@ ENTITY {
     },
     energy_usage = "550kW",
     mining_power = 1.5,
-    resource_searching_radius = 3.49,
+    resource_searching_radius = 4.49,
     vector_to_place_result = {0, -3.65},
     radius_visualisation_picture = {
         filename = "__base__/graphics/entity/electric-mining-drill/electric-mining-drill-radius-visualization.png",
