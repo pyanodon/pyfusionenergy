81b2e9274721a8c8e9e8109fe0cd27e6f19110f9
pyanodon <smylodon@gmail.com>
2020-03-11 21:34:47 -0300

    pollution
    Former-commit-id: 917e783127cee42fa00eb801d280ec75bb0291a6
    Former-commit-id: 1f0ae826d821cd25a2ebdbccc9a29d732c8b77c5

--- prototypes/buildings/xyhiphoe-pool-mk03.lua
+++ prototypes/buildings/xyhiphoe-pool-mk03.lua
@@ -50,7 +50,7 @@ ENTITY {
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
-        emissions_per_minute = -25,
+        emissions_per_minute = -35,
     },
     energy_usage = "1650kW",
     ingredient_count = 3,
