440b7b37833388c23cf0d51a28719e26bd97cd4c
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-08-16 03:51:44 -0400

    massive tweak to space science pack

---
+++ prototypes/updates/base-updates.lua
@@ -0,0 +1,40 @@
+
+--space science
+  RECIPE{
+    type = "recipe",
+    name = "satellite",
+    energy_required = 5,
+    enabled = false,
+    category = "crafting",
+    ingredients =
+    {
+      {"low-density-structure", 100},
+      {"fusion-reactor-mk01", 5},
+      {"processing-unit", 100},
+      {type = "fluid", name = "deuterium", amount = 200},
+      {type = "fluid", name = "helium3", amount = 100},
+      {type = "item", name = "blanket", amount = 1},
+      {type = "item", name = "divertor", amount = 3},
+      {type = "item", name = "reinforced-wall-shield", amount = 3},
+      {type = "item", name = "sc-unit", amount = 4},
+    },
+    result= "satellite",
+    requester_paste_multiplier = 1
+  }
+
+  RECIPE{
+    type = "recipe",
+    name = "rocket-part",
+    energy_required = 3,
+    enabled = false,
+    hidden = true,
+    category = "rocket-building",
+    ingredients =
+    {
+      {"rocket-control-unit", 10},
+      {"low-density-structure", 5},
+      {"super-alloy", 5},
+      {"rocket-fuel", 10}
+    },
+    result= "rocket-part"
+  }
