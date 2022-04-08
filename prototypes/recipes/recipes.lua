d8f16e45022dfc74e9483d80c07bd6029998142d
ShadowGlass0 <gkecskem@gmail.com>
2022-04-09 00:53:31 +1000

    Data files re-organization

--- prototypes/recipes/recipes.lua
+++ prototypes/recipes/recipes.lua
@@ -975,7 +975,7 @@ RECIPE {
     results = {
         {type = "item", name = "sc-coil", amount = 1}
     },
-}:add_ingredient({type = 'item', name = 're-tin', amount = 1})
+}
 
 RECIPE {
     type = "recipe",
@@ -1700,7 +1700,6 @@ RECIPE {
     main_product = "mox-fuel-cell",
 }:add_unlock("kovarex-enrichment-process-2")
 
-
 RECIPE {
     type = "recipe",
     name = "plutonium-fuel-reprocessing",
@@ -1718,3 +1717,121 @@ RECIPE {
     subgroup = "intermediate-product",
     order = "t",
 }:add_unlock("kovarex-enrichment-process-2")
+
+RECIPE {
+    type = "recipe",
+    name = "acetylene-combustion",
+    category = "combustion",
+    enabled = false,
+    energy_required = 3,
+    ingredients = {
+        {type = "fluid", name = "acetylene", amount = 100},
+        {type = "fluid", name = "water", amount = 100},
+        {type = "item", name = "coke", amount = 3}
+    },
+    results = {
+        {type = "fluid", name = "combustion-mixture1", amount = 150, temperature = 1600},
+        {type = "fluid", name = "steam", amount = 100, temperature = 120, catalyst_amount = 100}
+    },
+    icon = "__pycoalprocessinggraphics__/graphics/icons/combustion-acetylene.png",
+    icon_size = 32,
+    --main_product = "combustion-mixture1",
+    subgroup = "py-combustion",
+    order = "g"
+}:add_unlock('energy-3')
+
+RECIPE {
+    type = "recipe",
+    name = "calcium-carbide",
+    category = "hpf",
+    enabled = false,
+    energy_required = 4,
+    ingredients = {
+        {type = "item", name = "coke", amount = 7},
+        {type = "item", name = "lime", amount = 2}
+    },
+    results = {
+        {type = "item", name = "calcium-carbide", amount = 10}
+    }
+}:add_unlock("filtration-mk02")
+
+--GASOLINE to CM1
+RECIPE {
+    type = "recipe",
+    name = "gasoline-combustion",
+    category = "combustion",
+    enabled = false,
+    energy_required = 3,
+    ingredients = {
+        {type = "fluid", name = "gasoline", amount = 100},
+        {type = "fluid", name = "water", amount = 1000},
+        {type = "item", name = "coke", amount = 5},
+        {type = "item", name = "fuelrod-mk01", amount = 1}
+    },
+    results = {
+        {type = "fluid", name = "combustion-mixture1", amount = 150, temperature = 2600},
+        {type = "fluid", name = "steam", amount = 1000, temperature = 100, catalyst_amount = 1000}
+    },
+    icon = "__pycoalprocessinggraphics__/graphics/icons/combustion-gasoline.png",
+    icon_size = 32,
+    --main_product = "combustion-mixture1",
+    subgroup = "py-combustion",
+    order = "j"
+}:add_unlock("energy-3")
+
+if not mods['pyhightech'] then
+    RECIPE {
+        type = "recipe",
+        name = "iron-oxide",
+        category = "chemistry",
+        enabled = false,
+        energy_required = 5,
+        ingredients = {
+            {type = "fluid", name = "hydrogen-peroxide", amount = 100},
+            {type = "item", name = "iron-plate", amount = 5}
+        },
+        results = {
+            {type = "item", name = "iron-oxide", amount = 10}
+        }
+    }:add_unlock("sulfur-processing")
+end
+
+--space science
+RECIPE {
+    type = "recipe",
+    name = "satellite",
+    energy_required = 5,
+    enabled = false,
+    category = "advanced-crafting",
+    ingredients =
+    {
+      {"low-density-structure", 100},
+      {"fusion-reactor-mk01", 1},
+      {"processing-unit", 100},
+      {type = "fluid", name = "deuterium", amount = 200},
+      {type = "fluid", name = "helium3", amount = 100},
+      {type = "item", name = "blanket", amount = 1},
+      {type = "item", name = "divertor", amount = 3},
+      {type = "item", name = "reinforced-wall-shield", amount = 3},
+      {type = "item", name = "sc-unit", amount = 4},
+    },
+    result= "satellite",
+    requester_paste_multiplier = 1
+}
+
+RECIPE {
+    type = "recipe",
+    name = "rocket-part",
+    energy_required = 3,
+    enabled = false,
+    hidden = true,
+    category = "rocket-building",
+    ingredients =
+    {
+        {"rocket-control-unit", 10},
+        {"low-density-structure", 5},
+        {"super-alloy", 8},
+        {"rocket-fuel", 10}
+    },
+    result= "rocket-part"
+}
--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -22,6 +22,8 @@ TECHNOLOGY('rocket-silo'):add_prereq('super-alloy')
 
 TECHNOLOGY('space-science-pack'):add_prereq('fusion-mk02')
 
+TECHNOLOGY("production-science-pack"):remove_prereq("uranium-processing"):remove_prereq("speed-module-2"):remove_prereq("effectivity-module-2"):remove_prereq("robotics"):remove_prereq("filtration-mk02"):remove_prereq("energy-3"):add_prereq("nuclear-fuel-reprocessing"):add_prereq("diamond-mining")
+
 data.raw.technology["kovarex-enrichment-process"].unit =
 {
   ingredients =
@@ -42,43 +44,4 @@ fun.add_result("nuclear-fuel-reprocessing", {type = "item", name = "plutonium", 
 RECIPE("nuclear-fuel-reprocessing"):set_fields{energy_required = 30}
 RECIPE('guar-gum-plantation-mk04'):add_ingredient({type = "item", name = "super-alloy", amount = 20})
 RECIPE('compressor-mk01'):add_unlock('advanced-oil-processing')
-
---space science
-  RECIPE{
-    type = "recipe",
-    name = "satellite",
-    energy_required = 5,
-    enabled = false,
-    category = "advanced-crafting",
-    ingredients =
-    {
-      {"low-density-structure", 100},
-      {"fusion-reactor-mk01", 1},
-      {"processing-unit", 100},
-      {type = "fluid", name = "deuterium", amount = 200},
-      {type = "fluid", name = "helium3", amount = 100},
-      {type = "item", name = "blanket", amount = 1},
-      {type = "item", name = "divertor", amount = 3},
-      {type = "item", name = "reinforced-wall-shield", amount = 3},
-      {type = "item", name = "sc-unit", amount = 4},
-    },
-    result= "satellite",
-    requester_paste_multiplier = 1
-  }
-
-  RECIPE{
-    type = "recipe",
-    name = "rocket-part",
-    energy_required = 3,
-    enabled = false,
-    hidden = true,
-    category = "rocket-building",
-    ingredients =
-    {
-      {"rocket-control-unit", 10},
-      {"low-density-structure", 5},
-      {"super-alloy", 8},
-      {"rocket-fuel", 10}
-    },
-    result= "rocket-part"
-  }
+RECIPE('rocket-silo'):replace_ingredient("steel-plate", "super-alloy")
