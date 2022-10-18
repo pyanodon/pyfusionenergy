7b1de4912b53fcc775e5fdf790211362f81ecba5
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-10-17 22:27:17 -0600

    New rock generation

--- prototypes/ore/volcanic-pipe.lua
+++ prototypes/ore/volcanic-pipe.lua
@@ -1,4 +1,4 @@
-local resource_autoplace = require("resource-autoplace")
+local noise = require("noise")
 
 DATA {
     type = "autoplace-control",
@@ -35,19 +35,33 @@ ENTITY {
     selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
     tree_removal_probability = 0.7,
     tree_removal_max_distance = 32 * 32,
-    autoplace = resource_autoplace.resource_autoplace_settings {
+    autoplace = {
         name = "volcanic-pipe",
-        order = "b",
-        base_density = 3,
-        base_spots_per_km2 = 1.25,
-        random_probability = 1/48,
-        has_starting_area_placement = false,
-        random_spot_size_minimum = 1,
-        random_spot_size_maximum = 2,
-        additional_richness = 10000,
-        regular_rq_factor_multiplier = 1,
-        --starting_rq_factor_multiplier = 2,
-        --candidate_spot_count = 20
+        order = "b-volcanic-pipe",
+        -- We return the chance of spawning on any given tile here
+        probability_expression = noise.define_noise_function( function(x, y, tile, map)
+            -- This is the user's map setting for the frequency of this ore
+            local frequency_multiplier = noise.var("control-setting:volcanic-pipe:frequency:multiplier")
+            -- 0% chance of spawning in starting area (tier == 0)
+            -- Using this is equivalent to has_starting_area_placement = false
+            local tier = noise.clamp(noise.var("tier"), 0, 1)
+            -- 1 in 48 chunks (each chunk is 64x64 tiles)
+            local desired_frequency = 1 / (48 * 64^2)
+            -- Our final chance, likely a very, very small decimal
+            return tier * desired_frequency * frequency_multiplier
+          end),
+        -- We return the richness here, which is just the quantity the resource tile yields
+        richness_expression = noise.define_noise_function( function(x, y, tile, map)
+            -- This is the user's map setting for richness of this ore
+            -- We ignore size here because we're always a single tile resource
+            local richness_multiplier = noise.var("control-setting:volcanic-pipe:richness:multiplier")
+            -- This is the distance from the starting position, which is how vanilla scales ore yield
+            local distance_value = noise.var("distance")
+            -- This is our multiplier for the above, determining the yield gains over distance
+            local scalar = 2^16
+            -- Add it all together or what is likely a pretty big number
+            return distance_value * scalar * richness_multiplier
+        end)
     },
     stage_counts = {0},
     stages = {
