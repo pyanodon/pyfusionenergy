0a4484817f0758f3dac417cc6ef4df3bb880e219
notnotmelon <zacharypicco@gmail.com>
2023-10-16 17:20:07 -0500

    https://github.com/pyanodon/pybugreports/issues/320

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -10,15 +10,17 @@ local blacklist_effectivity = {
     "steam-exchange4"
 }
 
-if data.raw.module["effectivity-module"].limitation_blacklist == nil then
-    data.raw.module["effectivity-module"].limitation_blacklist = {}
-    data.raw.module["effectivity-module-2"].limitation_blacklist = {}
-    data.raw.module["effectivity-module-3"].limitation_blacklist = {}
-end
+if data.raw.module["effectivity-module"] then
+    if data.raw.module["effectivity-module"].limitation_blacklist == nil then
+        data.raw.module["effectivity-module"].limitation_blacklist = {}
+        data.raw.module["effectivity-module-2"].limitation_blacklist = {}
+        data.raw.module["effectivity-module-3"].limitation_blacklist = {}
+    end
 
---adding fusion recipes to effectivity modules black list
-for r, recipe in pairs(blacklist_effectivity) do
-    table.insert(data.raw.module["effectivity-module"].limitation_blacklist, recipe)
-    table.insert(data.raw.module["effectivity-module-2"].limitation_blacklist, recipe)
-    table.insert(data.raw.module["effectivity-module-3"].limitation_blacklist, recipe)
+    --adding fusion recipes to effectivity modules black list
+    for r, recipe in pairs(blacklist_effectivity) do
+        table.insert(data.raw.module["effectivity-module"].limitation_blacklist, recipe)
+        table.insert(data.raw.module["effectivity-module-2"].limitation_blacklist, recipe)
+        table.insert(data.raw.module["effectivity-module-3"].limitation_blacklist, recipe)
+    end
 end
