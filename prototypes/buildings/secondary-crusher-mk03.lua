8fa53b1db4f43eeb6fa5eb3f3b0b077bef2004af
notnotmelon <zacharypicco@gmail.com>
2024-06-03 16:06:36 -0500

    Fixed that the secondary crusher played audio when it was idle

--- prototypes/buildings/secondary-crusher-mk03.lua
+++ prototypes/buildings/secondary-crusher-mk03.lua
@@ -96,7 +96,6 @@ ENTITY {
     vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
     working_sound = {
         sound = {filename = "__pyfusionenergygraphics__/sounds/secondary-crusher.ogg", volume = 1.1},
-        idle_sound = {filename = "__pyfusionenergygraphics__/sounds/secondary-crusher.ogg", volume = 0.3},
         apparent_volume = 2.5
     }
 }
