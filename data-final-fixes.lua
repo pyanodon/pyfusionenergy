a490de3d078fa13125c98704898b2a42e1a6080f
kingarthur91 <arthurkoy_durbin@yahoo.com>
2024-09-13 00:24:21 -0400

    updated effectivity modules

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -10,17 +10,17 @@ local blacklist_effectivity = {
     "steam-exchange4"
 }
 
-if data.raw.module["effectivity-module"] then
-    if data.raw.module["effectivity-module"].limitation_blacklist == nil then
-        data.raw.module["effectivity-module"].limitation_blacklist = {}
-        data.raw.module["effectivity-module-2"].limitation_blacklist = {}
-        data.raw.module["effectivity-module-3"].limitation_blacklist = {}
+if data.raw.module["efficiency-module"] then
+    if data.raw.module["efficiency-module"].limitation_blacklist == nil then
+        data.raw.module["efficiency-module"].limitation_blacklist = {}
+        data.raw.module["efficiency-module-2"].limitation_blacklist = {}
+        data.raw.module["efficiency-module-3"].limitation_blacklist = {}
     end
 
     --adding fusion recipes to effectivity modules black list
     for r, recipe in pairs(blacklist_effectivity) do
-        table.insert(data.raw.module["effectivity-module"].limitation_blacklist, recipe)
-        table.insert(data.raw.module["effectivity-module-2"].limitation_blacklist, recipe)
-        table.insert(data.raw.module["effectivity-module-3"].limitation_blacklist, recipe)
+        table.insert(data.raw.module["efficiency-module"].limitation_blacklist, recipe)
+        table.insert(data.raw.module["efficiency-module-2"].limitation_blacklist, recipe)
+        table.insert(data.raw.module["efficiency-module-3"].limitation_blacklist, recipe)
     end
 end
