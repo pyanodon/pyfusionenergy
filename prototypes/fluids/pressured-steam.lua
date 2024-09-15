d456fffd813f90c7f7b3548b9814c0916ebea065
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-15 13:00:07 +0200

    KJ is kJ

--- prototypes/fluids/pressured-steam.lua
+++ prototypes/fluids/pressured-steam.lua
@@ -8,7 +8,7 @@ FLUID {
     base_color = {r = 1, g = 1, b = 1},
     flow_color = {r = 1, g = 1, b = 1},
     max_temperature = 5000,
-    heat_capacity = "25KJ",
+    heat_capacity = "25kJ",
     gas_temperature = 15,
     pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59,
