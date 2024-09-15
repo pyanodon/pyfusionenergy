f3d14ce02f15b1a725532106dda38fa1b232b8ff
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-15 12:41:59 +0200

    remove noise

--- prototypes/ore/regolites.lua
+++ prototypes/ore/regolites.lua
@@ -1,5 +1,3 @@
-local noise = require 'noise'
-
 data:extend{{
     type = "autoplace-control",
     name = "regolites",
@@ -8,10 +6,36 @@ data:extend{{
     category = "resource",
 }}
 
-data:extend{{
-    type = "noise-layer",
-    name = "regolites"
-}}
+data:extend{
+    {
+        type = "noise-expression",
+        name = "py_regolites_starting_area",
+        -- 0% chance of spawning in starting area (tier == 0)
+        -- Using this is equivalent to has_starting_area_placement = false
+        expression = "clamp(var('tier'), 0, 1)"
+    },
+    {
+        type = "noise-expression",
+        name = "py_regolites_desired_frequency",
+        -- 1 in 48 chunks (each chunk is 64x64 tiles)
+        expression = "1 / (48 * 64^2)"
+    },
+    {
+        -- We return the chance of spawning on any given tile here
+        type = "noise-expression",
+        name = "py_regolites",
+        -- Our final chance, likely a very, very small decimal
+        expression = [[
+            py_regolites_starting_area * py_regolites_desired_frequency * var("control-setting:regolites:frequency:multiplier")
+        ]]
+    },
+    {
+        -- We return the richness here, which is just the quantity the resource tile yields
+        type = "noise-expression",
+        name = "py_regolites_richness",
+        expression = "2^16 * var('distance') * var('control-setting:regolites:richness:multiplier')"
+    }
+}
 
 ENTITY {
     type = "resource",
@@ -41,29 +65,9 @@ ENTITY {
         order = "b-regolites",
         control = "regolites",
         -- We return the chance of spawning on any given tile here
-        probability_expression = noise.define_noise_function( function(x, y, tile, map)
-            -- This is the user's map setting for the frequency of this ore
-            local frequency_multiplier = noise.var("control-setting:regolites:frequency:multiplier")
-            -- 0% chance of spawning in starting area (tier == 0)
-            -- Using this is equivalent to has_starting_area_placement = false
-            local tier = noise.clamp(noise.var("tier"), 0, 1)
-            -- 1 in 48 chunks (each chunk is 64x64 tiles)
-            local desired_frequency = 1 / (48 * 64^2)
-            -- Our final chance, likely a very, very small decimal
-            return tier * desired_frequency * frequency_multiplier
-          end),
+        probability_expression = "py_regolites",
         -- We return the richness here, which is just the quantity the resource tile yields
-        richness_expression = noise.define_noise_function( function(x, y, tile, map)
-            -- This is the user's map setting for richness of this ore
-            -- We ignore size here because we're always a single tile resource
-            local richness_multiplier = noise.var("control-setting:regolites:richness:multiplier")
-            -- This is the distance from the starting position, which is how vanilla scales ore yield
-            local distance_value = noise.var("distance")
-            -- This is our multiplier for the above, determining the yield gains over distance
-            local scalar = 2^16
-            -- Add it all together or what is likely a pretty big number
-            return distance_value * scalar * richness_multiplier
-        end)
+        richness_expression = "py_regolites_richness"
     },
     stage_counts = {0},
     stages = {
