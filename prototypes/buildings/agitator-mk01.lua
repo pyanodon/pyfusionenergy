9a9bdcc51b547042ec81210e43213147be2dc703
kingarthur91 <arthurkoy_durbin@yahoo.com>
2024-09-15 03:19:16 -0400

    car impact -1

--- prototypes/buildings/agitator-mk01.lua
+++ prototypes/buildings/agitator-mk01.lua
@@ -137,7 +137,7 @@ ENTITY {
         },
         off_when_no_fluid_recipe = true
     },
-    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
+    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact-1.ogg", volume = 0.65},
     working_sound = {
         sound = {filename = "__pyfusionenergygraphics__/sounds/agitator.ogg", volume = 1.1},
         idle_sound = {filename = "__pyfusionenergygraphics__/sounds/agitator.ogg", volume = 0.3},
--- prototypes/buildings/jig-mk01.lua
+++ prototypes/buildings/jig-mk01.lua
@@ -190,7 +190,7 @@ ENTITY {
         },
         off_when_no_fluid_recipe = true
     },
-    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
+    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact-1.ogg", volume = 0.65},
     working_sound = {
         sound = {filename = "__pyfusionenergygraphics__/sounds/jig.ogg", volume = 1.2},
         idle_sound = {filename = "__pyfusionenergygraphics__/sounds/jig.ogg", volume = 0.3},
